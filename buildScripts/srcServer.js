2b198e6b6b459d1e1e4dd0a08e5bcef1e3f63203
Vanessa Barsotti <vbarsott@VanessaMac.local>
2019-10-19 17:39:26 -0400

    Create webserver program

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,30 @@
+// create variables for modules we're gonna use
+var express = require("express"); // webserver
+var path = require("path"); // manipule directories and files from my computer
+var open = require("open"); // open the browser
+
+// create a numeric variable for the port
+var port = 3000;
+
+// create (a webserver) a variable to be my webserver calling the express function that should be inside the express module of node_modules (app=webserver)
+var app = express();
+
+// tells the server how it should work for requests on the root path (/) of the site
+// when the server receives a request http get on the root it should execute the function to set the browser response
+// configura o servidor pra que quando ele receber um request get na raíz deverar retornar na resposta o que contem no arquivo index.html
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+// tells the server to listen on port 3000
+app.listen(port, function(err) {
+  // if error
+  if (err) {
+    // message error
+    console.log(err);
+    // if not error
+  } else {
+    // open the browser using the url with the open module
+    open("http://localhost:" + port);
+  }
+});
